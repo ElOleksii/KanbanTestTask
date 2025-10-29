6af9a1cab2123147e2e9eba7fc59a4820c575df1
Telenchak Oleksii <letel315@gmail.com>
2025-10-30 01:13:01 +0200

    fix pre state

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -4,8 +4,6 @@ import connectDB from "./config/db.js";
 import boardRoutes from "./routes/board.routes.js";
 import cardRoutes from "./routes/card.routes.js";
 import dotenv from "dotenv";
-import path from "path";
-import { fileURLToPath } from "url";
 
 dotenv.config();
 const app = express();
@@ -15,16 +13,6 @@ app.use(express.json());
 
 app.use("/api/boards", boardRoutes);
 app.use("/api/cards", cardRoutes);
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const frontendPath = path.join(__dirname, "../../frontend/dist");
-app.use(express.static(frontendPath));
-
-app.get("*", (_, res) => {
-  res.sendFile(path.join(frontendPath, "index.html"));
-});
 
 const PORT = process.env.PORT || 8000;
 
